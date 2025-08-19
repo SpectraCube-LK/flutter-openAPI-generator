a34ddee9fd3645da0c2fbef391d2373c9c5467b2
Steeban Charles <csteeh@gmail.com>
2025-08-19 16:26:31 +0530

    mock datas removed

--- lib/core/constants/settings.dart
+++
@@ -1,6 +0,0 @@
-class GlobalSettings {
-  static const String baseUrl = 'https://isharamadushan.lk/api';
-  static const String baseUrlForLinks = 'https://isharamadushan.lk/';
-  static const double courseImageAspectRatio =
-      16 / 9; // width : height ratio (16:9) for course images
-}
